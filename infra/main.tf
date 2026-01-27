7170eb0a973df97d5fd443f9e2af29763b9ebc03
Ben Kahl <ben.kahl2002@gmail.com>
2026-01-26 21:05:29 -0800

    fixed init issues

--- infra/main.tf
+++ infra/main.tf
@@ -2,16 +2,16 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.2.0"
+      version = "~> 6.28.0"
     }
   }
 
   backend "s3" {
-    bucket         = "gov-graph-state"
-    key            = "dev/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "gov-graph-lock"
+    bucket       = "gov-graph-state"
+    key          = "dev/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
 
