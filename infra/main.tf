d4d93afa523ce7b2b41518eee1aedf2b230a8292
Ben Kahl <ben.kahl2002@gmail.com>
2026-01-26 20:49:39 -0800

    changed auth methods for actions and added in s3 storage for tfstate along with a delete action

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp.aws"
+      version = "~> 6.2.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "gov-graph-state"
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "gov-graph-lock"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
+
 
 # -----------------------------------------------------------------------------
 # VPC
@@ -224,13 +242,13 @@ data "archive_file" "ingestion_zip" {
 
 # Lambda Function: Ingestion
 resource "aws_lambda_function" "ingestion_lambda" {
-  filename      = data.archive_file.ingestion_zip.output_path
-  function_name = "gov-graph-ingestion"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "ingest_contracts.main" # Assuming main() is the entry point
+  filename         = data.archive_file.ingestion_zip.output_path
+  function_name    = "gov-graph-ingestion"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "ingest_contracts.main" # Assuming main() is the entry point
   source_code_hash = data.archive_file.ingestion_zip.output_base64sha256
-  runtime       = "python3.9"
-  timeout       = 300
+  runtime          = "python3.9"
+  timeout          = 300
 
   vpc_config {
     subnet_ids         = module.vpc.private_subnets
@@ -246,3 +264,4 @@ resource "aws_lambda_function" "ingestion_lambda" {
     }
   }
 }
+
