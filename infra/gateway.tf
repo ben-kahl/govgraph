a6f3472867a2695aaf1bd738770c9c081a22d0f3
Ben Kahl <ben.kahl2002@gmail.com>
2026-02-23 15:44:04 -0800

    fmt fix

--- infra/gateway.tf
+++ infra/gateway.tf
@@ -1,17 +1,17 @@
 module "api_gateway" {
-  source = "terraform-aws-modules/apigateway-v2/aws"
+  source  = "terraform-aws-modules/apigateway-v2/aws"
   version = "~> 6.1.0"
 
-  name = "gov-graph-http"
-  description = "GovGraph HTTP API Gateway"
+  name          = "gov-graph-http"
+  description   = "GovGraph HTTP API Gateway"
   protocol_type = "HTTP"
 
   create_domain_name = false
   create_certificate = false
 
   cors_configuration = {
-  allow_headers = ["content-type", "x-amz-date", "authorization", "x-api-key", "x-amz-security-token", "x-amz-user-agent"]
-  allow_methods = ["*"]
-  allow_origins = ["*"]
+    allow_headers = ["content-type", "x-amz-date", "authorization", "x-api-key", "x-amz-security-token", "x-amz-user-agent"]
+    allow_methods = ["*"]
+    allow_origins = ["*"]
   }
 }
